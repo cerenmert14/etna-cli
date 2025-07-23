9f34081d87b43e321922c6a29216b1c55d84204e
Nikhil Kamath <nikhil.kamath@spacex.com>
2025-07-23 00:13:26 -0700

    added configs

--- workloads/OCaml/BST/lib/impl.ml
+++ workloads/OCaml/BST/lib/impl.ml
@@ -5,14 +5,14 @@ let rec insert (k : int) (v : int) (t : t) =
   | E -> T (E, k, v, E)
   | T (l, k', v', r) ->
       (*! insert *)
-(*!
       if k < k' then T (insert k v l, k', v', r)
       else if k' < k then T (l, k', v', insert k v r)
       else T (l, k', v, r)
-*)
       (*!! insert_1 *)
+      (*!
       let _ = ignore (l, k', v', r, insert) in
       T (E, k, v, E)
+      *)
       (*!! insert_2 *)
       (*!
       if k < k' then T ((insert k v l), k', v', r)
