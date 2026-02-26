920913d71ca4db0e5f384c513965671e66e9ce1f
Ceren Mert <cerenmert2003@gmail.com>
2026-02-26 01:33:29 -0500

    add thesis-experiments for reproducing experiments

--- steps.sh
+++ steps.sh
@@ -15,13 +15,13 @@ ETNA_DIR="." etna workload add Racket RBT --experiment exp3
 ETNA_DIR="." etna workload add Racket STLC --experiment exp3
 
 # run the experiment with different tests
-ETNA_DIR="." etna experiment run --name exp3 --tests bst # this runs all tests in `exp3/tests/bst.json`
-ETNA_DIR="." etna experiment run --name exp3 --tests bst --cross # this runs all tests in `exp3/tests/bst.json` with cross language mode.
+ETNA_DIR="." etna experiment run --name exp3 --tests bst-racket # this runs all tests in `exp3/tests/bst.json`
+ETNA_DIR="." etna experiment run --name exp3 --tests bst-racket --cross # this runs all tests in `exp3/tests/bst.json` with cross language mode.
 
-ETNA_DIR="." etna experiment run --name exp3 --tests rbt
+ETNA_DIR="." etna experiment run --name exp3 --tests rbt-racket
 ETNA_DIR="." etna experiment run --name exp3 --tests stlc-racket
 
 # visualize the results
-ETNA_DIR="." etna experiment visualize --name exp3 --figure bst --tests bst
-ETNA_DIR="." etna experiment visualize --name exp3 --figure rbt --tests rbt
-ETNA_DIR="." etna experiment visualize --name exp3 --figure stlc --tests stlc
+ETNA_DIR="." etna experiment visualize --name exp3 --figure bst --tests bst-racket
+ETNA_DIR="." etna experiment visualize --name exp3 --figure rbt --tests rbt-racket
+ETNA_DIR="." etna experiment visualize --name exp3 --figure stlc --tests stlc-racket
