61e02fb498436d48bdb3391c8bd12324d9e8816c
Ceren Mert <cerenmert2003@gmail.com>
2026-03-25 17:13:36 -0400

    readme and scripts

--- thesis-experiments/etna-stlc-qcheck.sh
+++ thesis-experiments/etna-stlc-qcheck.sh
@@ -1,20 +1,26 @@
 #!/bin/bash
 
-EXP_DIR="$PWD/exp3"
-GEN_FILE="$EXP_DIR/workloads/Racket/STLC/src/Generation.rkt" 
-STEPS_FILE="$EXP_DIR/workloads/Racket/STLC/steps.json"
-TEST_FILE="$EXP_DIR/tests/stlc-racket.json"
-STORE_ETNA="$PWD/thesis-experiments/rackcheck/etna-experiments"
+
+EXP_DIR="$PWD/exp1"
+WORKLOAD_DIR="$EXP_DIR/workloads/OCaml/STLC"
+GEN_FILE="$WORKLOAD_DIR/lib/Strategies/gen_bespoke_qcheck.ml" 
+STEPS_FILE="$EXP_DIR/workloads/OCaml/STLC/steps.json"
+TEST_FILE="$EXP_DIR/tests/stlc-ocaml.json"
+RUNNER_FILE="$WORKLOAD_DIR/bin/runner_qcheck.ml"
+STORE_ETNA="$PWD/thesis-experiments/qcheck/etna-experiments"
 
 # update files to run experiment
+rm $WORKLOAD_DIR/STLC.opam
 echo -n "" > $STEPS_FILE
 echo -n "" > $TEST_FILE
-cat $PWD/thesis-experiments/rackcheck/steps.json > $STEPS_FILE 
-cat $PWD/thesis-experiments/rackcheck/stlc-racket.json > $TEST_FILE
+echo -n "" > $RUNNER_FILE
+cat $PWD/thesis-experiments/qcheck/steps.json > $STEPS_FILE 
+cat $PWD/thesis-experiments/qcheck/stlc-ocaml.json > $TEST_FILE
+cat $PWD/thesis-experiments/qcheck/runner_qcheck.ml > $RUNNER_FILE
 
 # run & visualize the original experiment
-ETNA_DIR="." etna experiment run --name exp3 --tests stlc-racket
-ETNA_DIR="." etna experiment visualize --name exp3 --figure stlc --tests stlc-racket --visualization-type bucket
+ETNA_DIR="." etna experiment run --name exp1 --tests stlc-ocaml
+ETNA_DIR="." etna experiment visualize --name exp1 --figure stlc --tests stlc-ocaml --visualization-type bucket
 
 # save results to rackcheck/etna-experiments
 cp -r $EXP_DIR/figures $STORE_ETNA/original/
@@ -30,45 +36,44 @@ if [ ! -f "$GEN_FILE" ]; then
     exit 1
 fi
 
-# update generator 
-echo -n "" > $GEN_FILE
-cat $PWD/thesis-experiments/rackcheck/Generation.rkt > $GEN_FILE
-
 TYP_VALUES=(5 10 25 50 100 250)
 EXPR_VALUES=(5 10 15 20 25)
+
+# update generator 
+echo -n "" > $GEN_FILE
+cat $PWD/thesis-experiments/qcheck/gen_bespoke_qcheck.ml > $GEN_FILE
 
 for t in "${TYP_VALUES[@]}"; do
   for e in "${EXPR_VALUES[@]}"; do
     
     echo "Setting: gen:typ = $t, gen:expr = $e"
-    perl -i -pe "s/\(gen:typ \d+\)/(gen:typ $t)/" "$GEN_FILE"
-    perl -i -pe "s/\(gen:expr '\(\) tau \d+\)/(gen:expr '\(\) tau $e)/" "$GEN_FILE"
+    perl -i -pe "s/let\* typ = typGen \d+ in/let* typ = typGen $t in/" "$GEN_FILE"
+    perl -i -pe "s/go \d+ ctx t(\s*)$/go $e ctx t\1/" "$GEN_FILE"
 
     echo "============================================="
     echo "Running ETNA with gen:typ = $t, gen:expr = $e"
     echo "============================================="
 
-    ETNA_DIR="." etna experiment run --name exp3 --tests stlc-racket
+    ETNA_DIR="." etna experiment run --name exp1 --tests stlc-ocaml
 
     echo "============================================="
     echo "ETNA Completed run with: gen:typ = $t, gen:expr = $e"
     echo "============================================="
     
-    ETNA_DIR="." etna experiment visualize --name exp3 --figure stlc --tests stlc-racket --visualization-type bucket
+    ETNA_DIR="." etna experiment visualize --name exp1 --figure stlc --tests stlc-ocaml --visualization-type bucket
 
     echo "============================================="
     echo "ETNA is done visualizing with: gen:typ = $t, gen:expr = $e"
     echo "============================================"
-
+    
     # save results and reset
     cp -r $EXP_DIR/figures $STORE_ETNA/gen:typ-${t}--gen:expr-${e}/
     cat $EXP_DIR/store.jsonl > $STORE_ETNA/gen:typ-${t}--gen:expr-${e}/gen:typ-${t}--gen:expr-${e}-store.jsonl
-
-    echo -n "" > $EXP_DIR/store.jsonl
     
+    echo -n "" > $EXP_DIR/store.jsonl
   done
 done
 
 # reset generator
 echo -n "" > $GEN_FILE
-cat $PWD/thesis-experiments/rackcheck/Generation-OG.rkt > $GEN_FILE
+cat $PWD/thesis-experiments/qcheck/gen_bespoke_qcheck_og.ml > $GEN_FILE
--- thesis-experiments/tyche-stlc-qcheck.sh
+++ thesis-experiments/tyche-stlc-qcheck.sh
@@ -8,68 +8,68 @@ MAIN_FILE="$WORKLOAD_DIR/bin/main.ml"
 MAIN_DUNE="$WORKLOAD_DIR/bin/dune" 
 STORE_TYCHE="$PWD/thesis-experiments/qcheck/tyche-experiments"
 
-# rm $WORKLOAD_DIR/STLC.opam
+rm $WORKLOAD_DIR/STLC.opam
 
-# # update main.ml and dune to run experiment
-# echo -n "" > $MAIN_FILE
-# cat $PWD/thesis-experiments/qcheck/tyche_main.ml > $MAIN_FILE
-# echo -n "" > $MAIN_DUNE
-# cat $PWD/thesis-experiments/qcheck/dune > $MAIN_DUNE
-# echo -n "" > $WORKLOAD_DIR/lib/impl.ml
-# cat $PWD/thesis-experiments/qcheck/impl.ml > $WORKLOAD_DIR/lib/impl.ml
+# update main.ml and dune to run experiment
+echo -n "" > $MAIN_FILE
+cat $PWD/thesis-experiments/qcheck/tyche_main.ml > $MAIN_FILE
+echo -n "" > $MAIN_DUNE
+cat $PWD/thesis-experiments/qcheck/dune > $MAIN_DUNE
+echo -n "" > $WORKLOAD_DIR/lib/impl.ml
+cat $PWD/thesis-experiments/qcheck/impl.ml > $WORKLOAD_DIR/lib/impl.ml
 
-# echo "============================================="
-# echo "Running Tyche with original generator"
-# echo "============================================="
+echo "============================================="
+echo "Running Tyche with original generator"
+echo "============================================="
 
-# rm -rf $WORKLOAD_DIR/_build
+rm -rf $WORKLOAD_DIR/_build
 
-# dune build --root=$WORKLOAD_DIR 
-# dune exec --root=$WORKLOAD_DIR stlc -- --strategy=qcheck:bespoke --property=SinglePreserve
+dune build --root=$WORKLOAD_DIR 
+dune exec --root=$WORKLOAD_DIR stlc -- --strategy=qcheck:bespoke --property=SinglePreserve
 
-# # save results
-# cp $PWD/tyche-log.jsonl $STORE_TYCHE/original.jsonl
-
-
-# echo "==============================================="
-# echo "Tyche completed run with the original generator"
-# echo "==============================================="
+# save results
+cp $PWD/tyche-log.jsonl $STORE_TYCHE/original.jsonl
 
 
-# # --------- Experiments with updated generator with different sizes --------- ##
+echo "==============================================="
+echo "Tyche completed run with the original generator"
+echo "==============================================="
 
-# # update generator 
-# echo -n "" > $GEN_FILE
-# cat $PWD/thesis-experiments/qcheck/gen_bespoke_qcheck.ml > $GEN_FILE
 
-# TYP_VALUES=(5 10 25 50 100 250)
-# EXPR_VALUES=(5 10 15 20 25)
+# --------- Experiments with updated generator with different sizes --------- ##
 
-# cd $WORKLOAD_DIR
+# update generator 
+echo -n "" > $GEN_FILE
+cat $PWD/thesis-experiments/qcheck/gen_bespoke_qcheck.ml > $GEN_FILE
 
-# for t in "${TYP_VALUES[@]}"; do
-#   for e in "${EXPR_VALUES[@]}"; do
+TYP_VALUES=(5 10 25 50 100 250)
+EXPR_VALUES=(5 10 15 20 25)
+
+cd $WORKLOAD_DIR
+
+for t in "${TYP_VALUES[@]}"; do
+  for e in "${EXPR_VALUES[@]}"; do
     
-#     echo "Setting: gen:typ = $t, gen:expr = $e"
+    echo "Setting: gen:typ = $t, gen:expr = $e"
     
-#     perl -i -pe "s/typGen \d+/typGen $t/" "$GEN_FILE"
-#     perl -i -pe "s/go \d+ ctx t/go $e ctx t/" "$GEN_FILE"
+    perl -i -pe "s/typGen \d+/typGen $t/" "$GEN_FILE"
+    perl -i -pe "s/go \d+ ctx t/go $e ctx t/" "$GEN_FILE"
 
-#     echo "============================================="
-#     echo "Running tyche with gen:typ = $t, gen:expr = $e"
-#     echo "============================================="
+    echo "============================================="
+    echo "Running tyche with gen:typ = $t, gen:expr = $e"
+    echo "============================================="
 
-#     rm -rf _build
-#     dune build
-#     dune exec stlc -- --strategy=qcheck:bespoke --property=SinglePreserve
+    rm -rf _build
+    dune build
+    dune exec stlc -- --strategy=qcheck:bespoke --property=SinglePreserve
 
-#     echo "============================================="
-#     echo "Tyche completed run with: gen:typ = $t, gen:expr = $e"
-#     echo "============================================="
+    echo "============================================="
+    echo "Tyche completed run with: gen:typ = $t, gen:expr = $e"
+    echo "============================================="
     
-#     cp $PWD/tyche-log.jsonl $STORE_TYCHE/gen:typ-${t}--gen:expr-${e}.jsonl
-#   done
-# done
+    cp $PWD/tyche-log.jsonl $STORE_TYCHE/gen:typ-${t}--gen:expr-${e}.jsonl
+  done
+done
 
 # reset generator and main
 echo -n "" > $GEN_FILE
